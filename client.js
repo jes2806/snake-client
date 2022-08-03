0f6bd4097ab779171d7f27173a7f122aeb5e69e9
John Sweeney <jes2806@gmail.com>
2022-08-03 14:03:25 -0600

    pushing work to repo

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+const connect = function() {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: '50541'
+  });
+
+  conn.on('data', (data) => {
+    console.log(`server says: ${data}`);
+  });
+
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = connect;
